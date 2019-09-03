a764c9130a4171bc3d4f80545e15a6d976012e02
PacCol <pacome.colinet1@gmail.com>
2019-09-03 21:56:48 +0200

    La fonction uploader marche

--- index.php
+++ index.php
@@ -1,12 +1,57 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+<style>
+
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button2 {
+  background-color: white;
+  color: black;
+  border: 2px solid #008CBA;
+}
+
+.button2:hover {
+  background-color: #008CBA;
+  color: white;
+}
+
+
+</style>
+</head>
+
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
+  <h1>Cloud</h1>
+  <p><a class="button button2" href="uploader.html">Upload a new file</a></p>
+  <br>
+
+  <table style="width:100%">
+  <tr>
+    <th>File</th>
+    <th>Download</th>
+    <th>Delete</th>
+  </tr>
+
+<?php
+
+
+
+?>
 
 </body>
 </html>
